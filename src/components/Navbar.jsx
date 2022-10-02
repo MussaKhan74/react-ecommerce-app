980ea134df0d8cc0d848bf4e8054f5e63fff407b
MussaKhan74 <MarkeLoof@gmail.com>
2022-10-03 04:11:48 +0500

    added slider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>CATCHYN.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
