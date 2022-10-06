c6d51862f4c8dcb4a95ee56795c07b383554dcd6
MussaKhan74 <MarkeLoof@gmail.com>
2022-10-06 17:21:24 +0500

    added login and register pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 export default function App() {
-  return <Product />;
+  return <Login />;
 }
