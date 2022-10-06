25d8b48b9727357c24d9b4e4c78d1ca172b4d63e
MussaKhan74 <MarkeLoof@gmail.com>
2022-10-06 17:43:03 +0500

    added cart page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 export default function App() {
-  return <Login />;
+  return <Cart />;
 }
